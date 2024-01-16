bd8f923d8abacc26267e1a26410c5b36b7b13cb9
justinzhong7789 <justinzhong7789@gmail.com>
2024-01-16 15:12:39 -0800

    1. Added dsp module to ProJucer 2. Fixed a small issue caused by JUCE's new requirement that AudioParameters now need to have a ParameterID with version hints when using AudioUnits.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -193,19 +193,24 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 juce::AudioProcessorValueTreeState::ParameterLayout MyaudioappAudioProcessor::createParameterLayout()
 {
     juce::AudioProcessorValueTreeState::ParameterLayout layout;
-    layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq", "LowCut Freq",
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID { "LowCut Freq", 1 },
+                                                           "LowCut Freq",
                                                            juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
                                                            20.f));
-    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq", "HighCut Freq",
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID { "HighCut Freq", 1 },
+                                                           "HighCut Freq",
                                                            juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
                                                            20000.f));
-    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq", "Peak Freq",
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID { "Peak Freq", 1 },
+                                                           "Peak Freq",
                                                            juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
                                                            750.f));
-    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain", "Peak Gain",
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID { "Peak Gain", 1 },
+                                                           "Peak Gain",
                                                            juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
                                                            0.0f));
-    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality", "Peak Quality",
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID { "Peak Quality", 1 },
+                                                           "Peak Quality",
                                                            juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f),
                                                            1.f));
     juce::StringArray stringarray;
@@ -215,8 +220,8 @@ juce::AudioProcessorValueTreeState::ParameterLayout MyaudioappAudioProcessor::cr
         str << " db/Oct";
         stringarray.add(str);
     }
-    layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", stringarray, 0));
-    layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", stringarray, 0));
+    layout.add(std::make_unique<juce::AudioParameterChoice>(juce::ParameterID { "LowCut Slope", 1 }, "LowCut Slope", stringarray, 0));
+    layout.add(std::make_unique<juce::AudioParameterChoice>(juce::ParameterID { "HighCut Slope", 1 }, "HighCut Slope", stringarray, 0));
     return layout;
     
 }
